1090682e598001c1264e0191d005c102a248366a
GFrasson <bielfc27@gmail.com>
2024-06-10 17:59:16 -0300

    Remove unused files

--- Game.hs
+++
@@ -1,9 +0,0 @@
-module Game (startGame) where
-
-import Die (rollDie)
-
-startGame :: IO ()
-startGame = do
-  putStrLn "---- Jogo de dados ----"
-  let randomDie = rollDie
-  putStrLn $ show randomDie
--- Main.hs
+++
@@ -1,6 +0,0 @@
-module Main (main) where
-
-import Game (startGame)
-
-main :: IO ()
-main = startGame
