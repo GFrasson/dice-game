1acbf4b71d006ca4ae86429ec7d6cf3472b1eb29
GFrasson <bielfc27@gmail.com>
2024-06-19 15:54:15 -0300

    Base game structure

--- src/Die.hs
+++ src/Die.hs
@@ -1,9 +1,50 @@
-module Die (rollDie) where
+module Die (Die, rollDice, possibleRotations, oppositeFace, rotateDie, removeDie) where
 import System.Random (randomRIO)
 
-data Die = Die {
-  face :: Int
-} deriving Show
+data Die = Die Int deriving Show
 
-rollDie :: IO Int
-rollDie = randomRIO (1, 6) :: IO Int
+getFace :: Die -> Int
+getFace (Die face) = face
+
+minFace :: Int
+minFace = 1
+
+maxFace :: Int
+maxFace = 6
+
+isValidDie :: Die -> Bool
+isValidDie (Die face) = face >= minFace && face <= maxFace
+
+oppositeFace :: Die -> Int
+oppositeFace die
+  | isValidDie die = 7 - getFace die
+  | otherwise = 0
+
+possibleRotations :: Die -> [Int]
+possibleRotations die
+  | isValidDie die = [x | x <- [minFace..maxFace], x < getFace die && x /= oppositeFace die]
+  | otherwise = []
+
+rotateDie :: Int -> Int -> [Die] -> [Die]
+rotateDie _ _ [] = []
+rotateDie dieIndex newFace (x:xs)
+  | dieIndex == 0 = Die newFace:xs
+  | otherwise = x:rotateDie (dieIndex - 1) newFace xs
+
+removeDie :: [Die] -> [Die]
+removeDie [] = []
+removeDie (x:xs)
+  | getFace x == 1 = xs
+  | otherwise = x:removeDie xs
+
+rollDie :: IO Die
+rollDie = do
+  randomInteger <- randomRIO (minFace, maxFace) :: IO Int
+  return (Die randomInteger)
+
+rollDice :: Int -> IO [Die]
+rollDice 0 = return []
+rollDice amount = do
+  die <- rollDie
+  dice <- rollDice (amount - 1)
+  return (die : dice)
--- src/Game.hs
+++ src/Game.hs
@@ -1,9 +1,79 @@
 module Game (startGame) where
 
-import Die (rollDie)
+-- import Control.Monad.State
+-- import Data.Functor.Identity
+
+import Die (Die, rollDice, rotateDie, removeDie)
+import Text.Read (readMaybe)
+import Data.Maybe (fromJust)
+
+data Player = Person | Computer deriving (Show, Eq)
+
+data GameState = GameState {
+  diceTable :: [Die],
+  currentPlayer :: Player
+}
+
+initGameState :: [Die] -> GameState
+initGameState randomDice = GameState { diceTable = randomDice, currentPlayer = Person }
+
+togglePlayer :: Player -> Player
+togglePlayer player
+  | player == Person = Computer
+  | otherwise = Person
+
+-- rotateDieInState :: Int -> Int -> State GameState ()
+-- rotateDieInState dieIndex newFace = StateT
+--   $ \(GameState dice player)
+--   -> Identity ((), GameState (rotateDie dieIndex newFace dice) (togglePlayer player))
+
+rotateDieInState :: Int -> Int -> GameState -> GameState
+rotateDieInState dieIndex newFace (GameState dice player) =
+  GameState (rotateDie dieIndex newFace dice) (togglePlayer player)
+
+-- removeDieInState :: State GameState ()
+-- removeDieInState = StateT
+--   $ \(GameState dice player)
+--   -> Identity ((), GameState (removeDie dice) (togglePlayer player))
+
+removeDieInState :: GameState -> GameState
+removeDieInState (GameState dice player) = GameState (removeDie dice) (togglePlayer player)
+
+readDiceAmount :: IO Int
+readDiceAmount = do
+  putStrLn "Digite o numero de dados:"
+  diceAmountInput <- getLine
+  let diceAmount = readMaybe diceAmountInput :: Maybe Int
+
+  if diceAmount == Nothing then
+    readDiceAmount
+  else
+    return (fromJust diceAmount)
 
 startGame :: IO ()
 startGame = do
   putStrLn "---- Jogo de dados ----"
-  randomDie <- rollDie
-  putStrLn $ show randomDie
+
+  diceAmount <- readDiceAmount
+  randomDice <- rollDice diceAmount
+
+  let gameState = initGameState randomDice
+  gameEventLoop gameState
+
+gameEventLoop (GameState dice player) = do
+  -- Verify End Game
+
+  -- Show possible moves
+  
+  putStrLn "Escolha uma jogada:"
+
+  -- Show Dice
+  
+  putStrLn "Escolha um dado:"
+
+  -- Show possible rotations
+
+  putStrLn "Escolha a nova face para cima:"
+
+  -- Update GameState
+  -- Recursion
